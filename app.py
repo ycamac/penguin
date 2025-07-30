62dfb7ad96b2ca58de643f29dd09bc848dd711e2
ycamac <y.cama@outlook.com>
2025-07-30 08:58:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,3 +19,6 @@ with st.sidebar:
   island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
   bill_length_mm = st.slider('Bill length (mm)',32.1,59.6,43.9)
   bill_depth_mm = st.slider('Bill depth (mm)', 13.1,21.5,17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172,231,201)
+  body_mass_g = st.slider('Body mass (g)', 2700,6300,4207)
+  gender = st.selectbox('Gender', ('male','female')
