b8db9c9c4bb81d901a50dc19c5d829718f69b52f
ycamac <y.cama@outlook.com>
2025-07-30 08:31:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy
 import streamlit as st
-
+import sklearn.emsemble import RandomForestClassifier
 
 st.title('Penguine Specie Prediction ML: App')
+st.info('This is a end-to-end Machine Learning App')
