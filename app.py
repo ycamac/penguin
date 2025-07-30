ff72d425c1025b61e7917967059705f6d2f7f334
ycamac <y.cama@outlook.com>
2025-07-30 09:35:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -46,10 +46,10 @@ with st.sidebar:
   data = {
     'island': island,
     'bill_length_mm': bill_length_mm,
-    'bill_depth_mm':bill_depth_mm,
-    'flipper_length_mm'=flipper_length_mm,
-    'body_mass_g'=body_mass_g,
-    'gender'=gender    
+    'bill_depth_mm': bill_depth_mm,
+    'flipper_length_mm': flipper_length_mm,
+    'body_mass_g': body_mass_g,
+    'gender': gender    
   }
   input_df = pd.DataFrame(data, index = [0])
   input_penguins =  pd.concat([input_df, X_raw], axis=0)
